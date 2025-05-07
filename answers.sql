cc910d9223ad1a8bcc48cb738899021bed2a4cb3
marylizkarumba <marylizkarumba791@gmail.com>
2025-05-07 15:21:46 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,11 +1,8 @@
--- Create a new table in 1NF
 CREATE TABLE ProductDetail_1NF (
     OrderID INT,
     CustomerName VARCHAR(100),
-    Product VARCHAR(100)
-);
+    Product VARCHAR(100));
 
--- Insert data, splitting the Products column
 INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
 VALUES 
     (101, 'John Doe', 'Laptop'),
@@ -16,30 +13,25 @@ VALUES
     (103, 'Emily Clark', 'Phone');
 
 
- -- Create a new Customer table
 CREATE TABLE Customers (
     CustomerName VARCHAR(100),
-    PRIMARY KEY (CustomerName)
-);
+    PRIMARY KEY (CustomerName));
 
--- Create a new OrderDetails table in 2NF
+
 CREATE TABLE OrderDetails_2NF (
     OrderID INT,
     CustomerName VARCHAR(100),
     Product VARCHAR(100),
     Quantity INT,
     PRIMARY KEY (OrderID, Product),
-    FOREIGN KEY (CustomerName) REFERENCES Customers(CustomerName)
-);
+    FOREIGN KEY (CustomerName) REFERENCES Customers(CustomerName));
 
--- Insert data into Customers table
 INSERT INTO Customers (CustomerName)
 VALUES 
     ('John Doe'),
     ('Jane Smith'),
     ('Emily Clark');
 
--- Insert data into OrderDetails_2NF table
 INSERT INTO OrderDetails_2NF (OrderID, CustomerName, Product, Quantity)
 VALUES 
     (101, 'John Doe', 'Laptop', 2),
